a3e4d4be873a9cc7b8c6ec926dc6148f303d4457
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-10-19 15:19:14 +0200

    layout step

--- app.rb
+++ app.rb
@@ -7,10 +7,17 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do
-  erb :index
-end
+
 
 get '/about' do
   erb :about
+end
+
+get '/about' do
+  erb :view_name
+end
+
+get '/' do
+  @usernames = [ "ssaunier", "Papillard" ]
+  erb :index
 end
