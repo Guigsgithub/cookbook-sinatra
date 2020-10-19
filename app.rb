93f7ce4712370026c7cdcd78cc0e64762a2b4f20
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-10-19 16:19:11 +0200

    display name and description

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
-
-
 
 get '/about' do
   erb :about
@@ -18,6 +18,12 @@ get '/about' do
 end
 
 get '/' do
-  @usernames = [ "ssaunier", "Papillard" ]
+  @recipes = [ ["gateau", "chocolat"], ["tarte", "pomme" ]]
   erb :index
 end
+
+post '/recipe' do
+  "The recipe is #{params[:name]} and The description is #{params[:description]}."
+end
+
+
